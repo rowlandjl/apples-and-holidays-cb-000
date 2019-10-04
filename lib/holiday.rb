36becc01513e70c2fe1e2d404e5a6b83f8feaffc
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 22:15:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holiday|
+
+  end 
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
